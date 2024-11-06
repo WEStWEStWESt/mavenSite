448e3541b1719471594b1161dc7b9f1960ad4989
WESt <AlexWESt101087@mail.ru>
2024-11-06 21:35:16 +0300

    empty class Product added.

---
+++ buysell/src/main/java/com/example/buysell/models/Product.java
@@ -0,0 +1,10 @@
+package com.example.buysell.models;
+
+public class Product {
+    private String title;
+    private String description;
+    private int price;
+    private String city;
+    private String author;
+}
+
