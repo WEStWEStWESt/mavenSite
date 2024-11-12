ae5ba60fa9b584a97ed7b11b9db26eb8971c7afd
WESt <AlexWESt101087@mail.ru>
2024-11-12 23:05:53 +0300

    Project rebuilt in JDK21.

--- buysell/src/main/java/com/example/buysell/controllers/ProductController.java
+++ buysell/src/main/java/com/example/buysell/controllers/ProductController.java
@@ -1,13 +1,20 @@
 package com.example.buysell.controllers;
 
+import com.example.buysell.services.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class ProductController {
     //general page wit products
+    private final ProductService productService;
+
     @GetMapping("/")
-    public String products(){
+    public String products(Model model) {
+        model.addAttribute("products", productService.listProducts());
         return "products";
     }
 }
