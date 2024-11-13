20d684974a1dce4760d5402c4a1359a5c6792275
WESt <AlexWESt101087@mail.ru>
2024-11-13 23:00:31 +0300

    Functionality for working with products added.

--- buysell/src/main/java/com/example/buysell/controllers/ProductController.java
+++ buysell/src/main/java/com/example/buysell/controllers/ProductController.java
@@ -1,10 +1,13 @@
 package com.example.buysell.controllers;
 
+import com.example.buysell.models.Product;
 import com.example.buysell.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,5 +19,22 @@ public class ProductController {
     public String products(Model model) {
         model.addAttribute("products", productService.listProducts());
         return "products";
+    }
+
+    @GetMapping("/product/id")
+    public String productInfo(@PathVariable long id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        return "product-info";
+    }
+
+    @PostMapping("/product/create")
+    public String createProduct(Product product) {
+        productService.saveProduct(product);
+        return "redirect:/";
+    }
+    @PostMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return "redirect:/";
     }
 }
--- buysell/src/main/java/com/example/buysell/services/ProductService.java
+++ buysell/src/main/java/com/example/buysell/services/ProductService.java
@@ -8,11 +8,12 @@ import java.util.List;
 
 @Service
 public class ProductService {
-    private List<Product> products = new ArrayList<>();
+    private final List<Product> products = new ArrayList<>();
     private long ID = 0;
 
     {
-        products.add(new Product(++ID, "SP2", "Simple description", 1500, "Wherever", "iam"));
+        products.add(new Product(++ID, "Sony Playstation 5 ", "Simple description", 2500, "Wherever", "iam"));
+        products.add(new Product(++ID, "Iphone 12 ", "Simple description", 1350, "Wherever", "iam"));
     }
 
     public List<Product> listProducts() {
@@ -26,5 +27,12 @@ public class ProductService {
 
     public void deleteProduct(Long id) {
         products.removeIf(product -> product.getId().equals(id));
+    }
+
+    public Product getProductById(long id) {
+        for (Product product : products) {
+            if (product.getId().equals(id)){ return product;}
+        }
+        return null;
     }
 }
