9c47aec4d78a2bcfed0dd59385794da775404c23
WESt <AlexWESt101087@mail.ru>
2024-11-18 19:55:21 +0300

    The Database connected. The Methods Add, Search, Delete work.

--- buysell/src/main/java/com/example/buysell/services/ProductService.java
+++ buysell/src/main/java/com/example/buysell/services/ProductService.java
@@ -1,38 +1,36 @@
 package com.example.buysell.services;
 
 import com.example.buysell.models.Product;
+import com.example.buysell.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class ProductService {
-    private final List<Product> products = new ArrayList<>();
-    private long ID = 0;
+    private final ProductRepository productRepository;
 
-    {
-        products.add(new Product(++ID, "Sony Playstation 5 ", "Simple description", 2500, "Wherever", "iam"));
-        products.add(new Product(++ID, "Iphone 12 ", "Simple description", 1350, "Wherever", "iam"));
-    }
-
-    public List<Product> listProducts() {
-        return products;
+    public List<Product> listProducts(String title) {
+        if (title != null) {
+            return productRepository.findByTitle(title);
+        }
+        return productRepository.findAll();
     }
 
     public void saveProduct(Product product) {
-        product.setId(++ID);
-        products.add(product);
+        log.info("Saving new {}", product);
+        productRepository.save(product);
     }
 
     public void deleteProduct(Long id) {
-        products.removeIf(product -> product.getId().equals(id));
+        productRepository.deleteById(id);
     }
 
     public Product getProductById(long id) {
-        for (Product product : products) {
-            if (product.getId().equals(id)){ return product;}
-        }
-        return null;
+        return productRepository.findById(id).orElse(null);
     }
 }
